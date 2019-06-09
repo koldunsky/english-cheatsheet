99e2c5e5e3bf3c83318e9b205cec0ca325b2717f
Ruslan Koldunsky <koldunsky@gmail.ru>
2019-06-09 13:39:27 +0300

    Add tenses

---
+++ src/views/Tenses/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="tenses">
+    <div
+      v-for="time in ['past', 'present', 'future']"
+      :key="time"
+      class="time-block"
+      :class="`time-block__${time}`"
+    >
+
+      <div
+        class="tense"
+        v-for="tense in [['simple'], ['continuous'], ['perfect'], ['perfect', 'continuous'] ]"
+        :key="tense.join('_')"
+        :class="[`tense_${time}`, tense.map((t) => `tense_${t}`)]">
+        {{time}} {{tense.map((t) => t).join(' ')}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" src="./index.scss"></style>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  components: {
+  },
+  data: () => {
+    return {
+      times: ['past', 'present', 'future']
+    }
+  }
+})
+</script>
