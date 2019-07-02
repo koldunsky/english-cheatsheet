2157dcc8e4b56d11717f0c3377d0a68af1d42d04
Ruslan Koldunsky <koldunsky@gmail.ru>
2019-07-03 00:47:12 +0300

    Add verb converter

---
+++ src/utils/verbConverter.js
@@ -0,0 +1,38 @@
+import irregularVerbs from './irregularVerbs'
+
+export const isVowel = (char) => ['a', 'e', 'i', 'o', 'u'].indexOf(char.toLowerCase()) >= 0
+
+export const isShortVowel = (char) => ['i', 'o', 'u'].indexOf(char.toLowerCase()) >= 0
+
+export const getSecondForm = (verb) => {
+  let index = irregularVerbs.first.indexOf(verb)
+  const lastChar  = verb.charAt(verb.length - 1)
+
+  if (index !== -1) {
+    return irregularVerbs.second[i]
+  }
+
+  if (lastChar === 'e') {
+    return `${verb}d`
+  }
+
+  if (lastChar === 'y' && !isVowel(verb.charAt(verb.length - 2))) {
+    return `${verb.slice(0, -1)}ied`
+  }
+
+  if (lastChar === 'l') {
+    return `${verb}led`
+  }
+}
+
+export const getGerund = (verb) => `${verb}ing`
+
+export const getThirdForm = (verb) => {
+  return verb
+}
+
+export default (verb) => ({
+  presentSimple: verb,
+  presentContinuous: getGerund(verb),
+  presentPerfect: getThirdForm()
+})
