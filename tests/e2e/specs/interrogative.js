a8f0f7081537484aac87bd354333dee2971eda3e
Ruslan Koldunsky <ruslan.koldunsky@xing.com>
2020-01-12 22:01:47 +0100

    Tests for negative and interrogative phrase forms

--- tests/e2e/specs/interrogative.js
+++ tests/e2e/specs/interrogative.js
@@ -1,7 +1,7 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('Interrogative phrases are correct', () => {
-  it('Interrogative phrases are correct', () => {
+  it('1st person', () => {
     cy.visit('/')
     cy.get('.select.phrase-form').select('interrogative')
     // Past
@@ -44,6 +44,144 @@ describe('Interrogative phrases are correct', () => {
     })
     cy.get('.tense_future-perfect-continuous .content').should(($div) => {
       expect($div.get(0).innerText).to.eq('Will I have been chilling?')
+    })
+  })
+  it('2nd person', () => {
+    cy.visit('/')
+    cy.get('.select.pronoun').select('you')
+    cy.get('.select.phrase-form').select('interrogative')
+    // Past
+    cy.get('.tense_past-simple .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Did you chill?')
+    })
+    cy.get('.tense_past-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Were you chilling?')
+    })
+    cy.get('.tense_past-perfect .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Had you chilled?')
+    })
+    cy.get('.tense_past-perfect-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Had you been chilling?')
+    })
+
+    // Present
+    cy.get('.tense_present-simple .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Do you chill?')
+    })
+    cy.get('.tense_present-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Are you chilling?')
+    })
+    cy.get('.tense_present-perfect .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Have you chilled?')
+    })
+    cy.get('.tense_present-perfect-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Have you been chilling?')
+    })
+
+    // Future
+    cy.get('.tense_future-simple .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Will you chill?')
+    })
+    cy.get('.tense_future-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Will you be chilling?')
+    })
+    cy.get('.tense_future-perfect .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Will you have chilled?')
+    })
+    cy.get('.tense_future-perfect-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Will you have been chilling?')
+    })
+  })
+  it('3rd person', () => {
+    cy.visit('/')
+    cy.get('.select.pronoun').select('he/she/it')
+    cy.get('.select.phrase-form').select('interrogative')
+    // Past
+    cy.get('.tense_past-simple .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Did it chill?')
+    })
+    cy.get('.tense_past-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Was it chilling?')
+    })
+    cy.get('.tense_past-perfect .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Had it chilled?')
+    })
+    cy.get('.tense_past-perfect-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Had it been chilling?')
+    })
+
+    // Present
+    cy.get('.tense_present-simple .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Does it chill?')
+    })
+    cy.get('.tense_present-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Is it chilling?')
+    })
+    cy.get('.tense_present-perfect .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Has it chilled?')
+    })
+    cy.get('.tense_present-perfect-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Has it been chilling?')
+    })
+
+    // Future
+    cy.get('.tense_future-simple .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Will it chill?')
+    })
+    cy.get('.tense_future-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Will it be chilling?')
+    })
+    cy.get('.tense_future-perfect .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Will it have chilled?')
+    })
+    cy.get('.tense_future-perfect-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Will it have been chilling?')
+    })
+  })
+  it('Plural', () => {
+    cy.visit('/')
+    cy.get('.select.pronoun').select('they')
+    cy.get('.select.phrase-form').select('interrogative')
+    // Past
+    cy.get('.tense_past-simple .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Did they chill?')
+    })
+    cy.get('.tense_past-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Were they chilling?')
+    })
+    cy.get('.tense_past-perfect .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Had they chilled?')
+    })
+    cy.get('.tense_past-perfect-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Had they been chilling?')
+    })
+
+    // Present
+    cy.get('.tense_present-simple .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Do they chill?')
+    })
+    cy.get('.tense_present-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Are they chilling?')
+    })
+    cy.get('.tense_present-perfect .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Have they chilled?')
+    })
+    cy.get('.tense_present-perfect-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Have they been chilling?')
+    })
+
+    // Future
+    cy.get('.tense_future-simple .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Will they chill?')
+    })
+    cy.get('.tense_future-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Will they be chilling?')
+    })
+    cy.get('.tense_future-perfect .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Will they have chilled?')
+    })
+    cy.get('.tense_future-perfect-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq('Will they have been chilling?')
     })
   })
 })
--- tests/e2e/specs/negative.js
+++ tests/e2e/specs/negative.js
@@ -138,4 +138,50 @@ describe('Negative phrases are correct', () => {
       expect($div.get(0).innerText).to.eq(`It won't have been chilling`)
     })
   })
+  it('Plural', () => {
+    cy.visit('/')
+    cy.get('.select.pronoun').select('they')
+    cy.get('.select.phrase-form').select('negative')
+    // Past
+    cy.get('.tense_past-simple .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq(`They didn't chill`)
+    })
+    cy.get('.tense_past-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq(`They weren't chilling`)
+    })
+    cy.get('.tense_past-perfect .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq(`They hadn't chilled`)
+    })
+    cy.get('.tense_past-perfect-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq(`They hadn't been chilling`)
+    })
+
+    // Present
+    cy.get('.tense_present-simple .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq(`They don't chill`)
+    })
+    cy.get('.tense_present-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq(`They are not chilling`)
+    })
+    cy.get('.tense_present-perfect .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq(`They haven't chilled`)
+    })
+    cy.get('.tense_present-perfect-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq(`They haven't been chilling`)
+    })
+
+    // Future
+    cy.get('.tense_future-simple .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq(`They won't chill`)
+    })
+    cy.get('.tense_future-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq(`They won't be chilling`)
+    })
+    cy.get('.tense_future-perfect .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq(`They won't have chilled`)
+    })
+    cy.get('.tense_future-perfect-continuous .content').should(($div) => {
+      expect($div.get(0).innerText).to.eq(`They won't have been chilling`)
+    })
+  })
 })
