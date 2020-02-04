e05291d35d8e910f41817a581137b1ba99d00e3e
Ruslan Koldunsky <koldunsky@gmail.ru>
2020-02-04 22:43:49 +0100

    Add words for making model

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import groupBy from 'lodash/groupBy';
+import { firstForm, secondForm, continuousForm } from '../DAS_MODEL/verbs'
+import irregularVerbs from './verbConverterCore/irregularVerbsList'
+
 Vue.config.productionTip = false
 
 new Vue({
@@ -10,3 +14,27 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+const verbsThatDoubled:Array<string> = []
+
+firstForm.forEach((word, i) => {
+  const secondFormOfCurrent = secondForm[i]
+  let firstIsDoubled = false
+  let secondIsDoubled = false
+  secondFormOfCurrent.split('').forEach(function (val, i, arr) {
+    if (val === arr[i + 1]) {
+      secondIsDoubled = true
+    }
+  })
+  word.split('').forEach(function (val, i, arr) {
+    if (val === arr[i + 1]) {
+      firstIsDoubled = true
+    }
+  })
+
+  if (secondIsDoubled && !firstIsDoubled) {
+    verbsThatDoubled.push(secondFormOfCurrent)
+  }
+})
+
+console.info(verbsThatDoubled)
